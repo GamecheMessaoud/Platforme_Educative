d4f955687a14f40c3343c47dd792e64e992d122e
Gameche Messaoud <gamechemessaoud@gmail.com>
2026-08-15 15:03:26 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,4 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
 
-// export default defineConfig(({ command }) => ({
-//   plugins: [react(), tailwindcss()],
-//   base: '/',
-// }))
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
